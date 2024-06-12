2372474c6df44c2eff98b0360e6b0f38e1b3c564
infoYaguarete <grupo.skipper@gmail.com>
2024-06-11 22:33:15 -0400

    first commit

---
+++ inversionCadenas.py
@@ -0,0 +1,9 @@
+cadena = input('Ingrese la cadena a invertir:')
+candena_invertida = '' # Variable donde se va almacenar la nueva cadena invertida
+
+#Este ciclo itera sobre cada caracter en la cadena original
+for caracter in cadena:
+    #En cada iteracion, el caracter actual se agrega al principio de 'cadena_invertida'
+    candena_invertida = caracter + candena_invertida
+
+print('La cadena invertida es:',candena_invertida)
