d37f5a1dd0d8801ec0520481cb42e95685f0c46a
infoYaguarete <grupo.skipper@gmail.com>
2024-06-23 19:31:16 -0400

    commit 10

---
+++ suma.cpp
@@ -0,0 +1,23 @@
+#include <iostream> // Para las entradas y salidas
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n1;
+    int n2;
+    int suma;
+
+    cout << "Ingresa un numero: ";
+    cin >> n1;
+    cout << "Ingresa otro numero: ";
+    cin >> n2;
+
+    suma = n1+n2;
+
+    cout << "La suma es de: " << suma << endl;
+
+
+    
+    return 0;
+}
